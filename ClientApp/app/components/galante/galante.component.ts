09550f409b28d9451209aec825298f8fca0f253e
Fabio Galante <fabiogalantemans@gmail.com>
2017-04-27 15:34:24 -0300

    Commit inicial

---
+++ ClientApp/app/components/galante/galante.component.ts
@@ -0,0 +1,17 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-name',
+    templateUrl: './galante.component.html'    
+    
+})
+export class GalanteComponent {
+
+
+    name: string;
+
+    constructor() {
+        this.name = 'Sam';
+    }
+}
